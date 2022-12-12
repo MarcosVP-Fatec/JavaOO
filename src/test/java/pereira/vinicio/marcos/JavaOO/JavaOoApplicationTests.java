259a2609e56eeda22ec0c39d46a289610d334a40
MarcosVP-Fatec <marcos.vinicio.per@gmail.com>
2022-12-12 02:12:20 +0000

    Serviço

--- src/test/java/pereira/vinicio/marcos/JavaOO/JavaOoApplicationTests.java
+++ src/test/java/pereira/vinicio/marcos/JavaOO/JavaOoApplicationTests.java
@@ -1,9 +1,10 @@
 package pereira.vinicio.marcos.JavaOO;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = JavaOoApplicationTests.class)
 class JavaOoApplicationTests {
 
 	@Test
