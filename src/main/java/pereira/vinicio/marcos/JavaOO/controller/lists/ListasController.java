8d6f81eb4881c431dcedda02816806ca56f842ee
MarcosVP-Fatec <marcos.vinicio.per@gmail.com>
2022-12-12 15:30:57 +0000

    Lista 1

--- src/main/java/pereira/vinicio/marcos/JavaOO/controller/lists/ListasController.java
+++ src/main/java/pereira/vinicio/marcos/JavaOO/controller/lists/ListasController.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,18 +21,30 @@ import pereira.vinicio.marcos.JavaOO.model.entity.Cargo;
 @CrossOrigin
 public class ListasController {
 
-	// @Autowired
-	// ListasService listasService;
-
+    private EntityManager em;
+    
     @GetMapping(value = "/salarios-beneficios/{ano}/{mes}/{funcionarios}/")
     public List<Cargo> salariosBeneficios(@PathVariable String ano
                                     ,@PathVariable String mes
                                     ,@PathVariable Long funcionarios[]
-                                    ){
+                                    ) throws Exception {
         System.out.println("Lista Salários Benefícios " + mes.toString() + "/" + ano.toString() + " : " + Arrays.asList(funcionarios));                                        
         List<Cargo> cargos = new ArrayList<>();
 
-cargos.add(new Cargo(1L));
+        try {
+            //TypedQuery<Cargo> query = em.createQuery( "select c from Cargo c",Cargo.class);
+            Query query = em.createNativeQuery( "select c from cargo c",Cargo.class);
+            cargos = query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            cargos = new ArrayList<>();
+            Cargo c = new Cargo(1L);
+//System.out.println(e.getMessage());            
+            //c.setDescr(e.getMessage().substring(0,20));
+            cargos.add(c);
+
+        }
+
         return cargos;
     }
 
