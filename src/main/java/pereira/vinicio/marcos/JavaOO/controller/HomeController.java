fb46ee090a27152256ddf136ab9ff6c6dd8f13ca
MarcosVP-Fatec <marcos.vinicio.per@gmail.com>
2022-12-12 05:49:56 +0000

    HomeController

--- src/main/java/pereira/vinicio/marcos/JavaOO/controller/HomeController.java
+++ src/main/java/pereira/vinicio/marcos/JavaOO/controller/HomeController.java
@@ -17,7 +17,12 @@ public class HomeController {
 
         String body = "<h1>Java Orientado a Objetos</h1><br><br>"
                     + "<h2>Rotas para teste:</h2><br><ol>"
-                    + "<li>Lista Func, ano, mês, Salário, benefício:<br>/JavaOO/listasalariosbeneficios/</li>"
+                    + "<li>Método que recebe uma lista de funcionários, mês e ano e retorna o valor total pago (salário e benefício) a esses funcionários no mês:<br><b>/JavaOO/lista/salarios-beneficios/</b></li>"
+                    + "<li>Método que recebe uma lista de funcionários, mês e ano e retorna o total pago somente em salários no mês:<br><b>/JavaOO/lista/salarios-no-mes/</b></li>"
+                    + "<li>Método que recebe uma lista somente com os funcionários que recebem benefícios, mês e ano e retorne o total pago em benefícios no mês:<br><b>/JavaOO/lista/beneficios-no-mes/</b></li>"
+                    + "<li>Método que recebe uma lista de funcionários, mês e ano e retorne o que recebeu o valor mais alto no mês:<br><b>/JavaOO/lista/maior-recebimento-no-mes/</b></li>"
+                    + "<li>Método que recebe uma lista somente com os funcionários que recebem benefícios, mês e ano e retorne o nome do funcionário que recebeu o valor mais alto em benefícios no mês:<br><b>/JavaOO/lista/maior-beneficio-no-mes/</b></li>"
+                    + "<li>Método que recebe uma lista de vendedores, mês e ano e retorne o que mais vendeu no mês.:<br><b>/JavaOO/lista/maior-venda-no-mes/</b></li>"
                     + "</ol>";
 
         return new ResponseEntity<String>(body, HttpStatus.OK);
